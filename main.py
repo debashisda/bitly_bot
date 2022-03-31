231b998ee1beed1a79852cb0baa50e32890332ca
Debashis Das <47478624+debashisda@users.noreply.github.com>
2022-03-31 12:06:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ def short_URL(long_url):
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     driver = webdriver.Chrome(executable_path=r'C:/chromedriver.exe', options=options)
     driver.get("https://bitly.com")  
-    driver.find_element_by_id("shorten_url").send_keys(long_url)  
-    #driver.find_element_by_id("shorten_btn").click()
-    element = driver.find_element_by_id('shorten_btn')
-    driver.execute_script("arguments[0].click();", element)
+    driver.find_element_by_id("shorten_url").send_keys(long_url)
+    driver.execute_script("arguments[0].click();", driver.find_element_by_id('shorten_btn'))
     driver.get("https://www.google.com")    
-    driver.execute_script("window.history.go(-1)")
+    driver.execute_script("window.history.go(-1);")
     cookie = driver.get_cookie('anon_shortlinks')
     driver.close()   
     print("Shortened URL: " + cookie['value'])    
@@ -23,12 +21,7 @@ def main():
     long_url = str(input("Enter Long URL: "))
     if long_url[long_url.find("bit.ly"):14] == "bit.ly":
         print("URL is already shortened!")
-    elif long_url[0:3] == "www":
-        long_url = "https://" + long_url
-        #print(long_url)
-        short_URL(long_url)
-    else:
-        #print("else" + long_url)        
+    else:       
         short_URL(long_url)
                  
 if __name__ == "__main__":
